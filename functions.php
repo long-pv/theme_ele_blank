b89485a5a9404837908d01aacce02eac5e7182ca
longpv <long.phamvan@adamosoft.com>
2025-09-11 09:04:58 +0700

    theme siêu sạch

--- functions.php
+++ functions.php
@@ -18,9 +18,9 @@ if (!defined('WP_MAX_MEMORY_LIMIT')) {
 function child_theme_scripts()
 {
     // slick
-    wp_enqueue_style('child_theme-style-slick-theme', CHILD_URI . '/assets/inc/slick/slick-theme.css', array(), _S_VERSION);
-    wp_enqueue_style('child_theme-style-slick', CHILD_URI . '/assets/inc/slick/slick.css', array(), _S_VERSION);
-    wp_enqueue_script('child_theme-script-slick', CHILD_URI . '/assets/inc/slick/slick.min.js', array('jquery'), _S_VERSION, true);
+    // wp_enqueue_style('child_theme-style-slick-theme', CHILD_URI . '/assets/inc/slick/slick-theme.css', array(), _S_VERSION);
+    // wp_enqueue_style('child_theme-style-slick', CHILD_URI . '/assets/inc/slick/slick.css', array(), _S_VERSION);
+    // wp_enqueue_script('child_theme-script-slick', CHILD_URI . '/assets/inc/slick/slick.min.js', array('jquery'), _S_VERSION, true);
 
     // add custom main css/js
     $main_css_file_path = CHILD_PATH . '/assets/css/main.css';
@@ -54,29 +54,29 @@ function write_log($log = null, $title = 'Debug')
 
 // Tạo menu theme settings chung
 // Setup theme setting page
-if (function_exists('acf_add_options_page')) {
-    // Trang cài đặt chính
-    acf_add_options_page(array(
-        'page_title' => 'Theme Settings',
-        'menu_title' => 'Theme Settings',
-        'menu_slug'  => 'theme-settings',
-        'capability' => 'edit_posts',
-        'redirect'   => false,
-        'position'   => 80
-    ));
-}
+// if (function_exists('acf_add_options_page')) {
+//     // Trang cài đặt chính
+//     acf_add_options_page(array(
+//         'page_title' => 'Theme Settings',
+//         'menu_title' => 'Theme Settings',
+//         'menu_slug'  => 'theme-settings',
+//         'capability' => 'edit_posts',
+//         'redirect'   => false,
+//         'position'   => 80
+//     ));
+// }
 // end
 
 // stop upgrading ACF pro plugin
-add_filter('site_transient_update_plugins', 'disable_plugins_update');
-function disable_plugins_update($value)
-{
-    // disable acf pro
-    if (isset($value->response['advanced-custom-fields-pro/acf.php'])) {
-        unset($value->response['advanced-custom-fields-pro/acf.php']);
-    }
-    return $value;
-}
+// add_filter('site_transient_update_plugins', 'disable_plugins_update');
+// function disable_plugins_update($value)
+// {
+//     // disable acf pro
+//     if (isset($value->response['advanced-custom-fields-pro/acf.php'])) {
+//         unset($value->response['advanced-custom-fields-pro/acf.php']);
+//     }
+//     return $value;
+// }
 
 // load widgets library by elementor
 function load_custom_widgets()
@@ -87,9 +87,9 @@ add_action('elementor/init', 'load_custom_widgets');
 // end
 
 // tối đa revision
-add_filter('wp_revisions_to_keep', function ($num, $post) {
-    return 5;
-}, 10, 2);
+// add_filter('wp_revisions_to_keep', function ($num, $post) {
+//     return 5;
+// }, 10, 2);
 
 add_shortcode('bcn_display', function () {
     if (function_exists('bcn_display')) {
@@ -113,4 +113,4 @@ function hide_yoast_seo_filters()
     </style>
 <?php
 }
-add_action('admin_head', 'hide_yoast_seo_filters');
+// add_action('admin_head', 'hide_yoast_seo_filters');
